68e183e1f8b93790c1d7e2afb95c6477d29775aa
Navneet Rajput <108276935+NavneetRajput1@users.noreply.github.com>
2023-01-25 21:39:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  if(input_digit == ""){
+        return [];
+    }
+    let table = [ '0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+     
+    let res =[];
+    let que = [''];
+     
+    while(que.length>0){
+        let str = que[0];
+        que.shift();
+         
+        if(str.length == input_digit.length){
+            res.push(str); // if all input_digit are replaced with char push to result
+        } else{
+//             get the current number from the input_digit i.e if str.length = 2 , input_digit =123 s= 3
+            let s= Number(input_digit.charAt(str.length));
+            let val = table[s]; // get char from the table i.e def for s =3
+             
+            for(i=0;i<val.length;i++){
+                que.push(str+val.charAt(i));
+            }
+        }
+    }
+     
+    return res;
+     
 }
 
 module.exports = letterCombinations;
